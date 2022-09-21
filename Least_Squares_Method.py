b2d781ba8bfb4335d7e89f70c7d639bf7394cd10
Ramtin Moslemi <76493699+RamtinMoslemi@users.noreply.github.com>
2022-09-21 19:31:57 +0430

    cleaned up a few things

--- Least_Squares_Method.py
+++ Least_Squares_Method.py
@@ -2,51 +2,66 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def f(n):
-    # create matrix X from x coordinates
-    X = np.matrix([x ** i for i in range(n + 1)]).transpose()
-    XTX = X.transpose() @ X
-    XTy = X.transpose() @ y
-    # calculate polynomial coefficients
-    Beta = np.linalg.inv(XTX) @ XTy
-    yHat = X @ Beta
-    y_yHat = y - yHat
-    MSE = y_yHat.transpose() @ y_yHat
-    return np.array(Beta), MSE.item()/len(y)
+def f(degree):
+    # create the design matrix from x coordinates
+    design_matrix = np.matrix([x ** i for i in range(degree + 1)]).transpose()
+    # figure out the polynomial coefficients
+    parameter_vector = np.linalg.inv(design_matrix.transpose() @ design_matrix) @ design_matrix.transpose() @ y
+    # calculate the residual vector
+    residual_vector = y - design_matrix @ parameter_vector
+    # compute the Mean Squared Error
+    mean_squared_error = (residual_vector.transpose() @ residual_vector).item() / n
+    # return the coefficients and the MSE
+    return np.array(parameter_vector), mean_squared_error
 
 
-def get_estimate(n, coefficients, point_x):
+# return the estimated value of output y given input x
+def get_estimate(degree, coefficients, point_x):
     point_y = 0
-    for i in range(n + 1):
-        point_y += coefficients[i] * point_x**i
+    for i in range(degree + 1):
+        point_y += coefficients[i] * point_x ** i
     return point_y
 
 
-def draw_graph(n, coefficients, rounding_factor=1):
-    points = np.linspace(min(x) - 0.1, max(x) + 0.1, 200)
-    eq = 'f_' + str(n) + '(x)= '
-    for i in range(n+1):
+def get_equation_str(degree, coefficients, mse, rounding_factor):
+    eq = 'f_' + str(degree) + '(x)= '
+    for i in range(degree + 1):
         eq += str(np.round(coefficients[i][0], rounding_factor))
         if i == 1:
             eq += 'x'
         elif i != 0 and i != 1:
             eq += 'x^' + str(i)
-        if i != n:
+        if i != degree:
             eq += ' + '
+    eq += 3 * '\;' + 'MSE=' + str(np.round(mse, rounding_factor))
+    return eq
+
+
+def draw_graph(degree, coefficients, mse, rounding_factor=3):
+    # set the points up
+    points = np.linspace(min(x) - 0.1, max(x) + 0.1, 200)
+    # get the equation string
+    eq = get_equation_str(degree, coefficients, mse, rounding_factor)
+    # add the title to the plot
     plt.title(r'$' + eq + '$')
-    plt.plot(points, get_estimate(n, coefficients, points))
+    # add the original inputs and outputs to the plot
     plt.plot(x, y, 'r*')
+    # draw the graph of our estimation
+    plt.plot(points, get_estimate(degree, coefficients, points))
+    # add grid to the plot
     plt.grid()
+    # show the plot
     plt.show()
-    print(eq, end=' ')
 
 
 if __name__ == '__main__':
-    y = np.matrix([1, 0, 2, -1]).transpose()
-    x = np.array([1, 2, -1, 0])
-    show_sol()
-    show1and2()
-    for degree in range(len(y)):
-        Beta, MSE = f(degree)
-        draw_graph(degree, Beta)
-        print('\t' * 2 * (len(y) - degree), 'MSE =', MSE)
+    # take in the number of inputs and outputs
+    n = int(input('Enter the number of inputs: '))
+    # create the x vector
+    x = np.array([int(i) for i in input('enter the inputs: ').split()])
+    # create the y vector
+    y = np.matrix([int(i) for i in input('enter the outputs: ').split()]).transpose()
+    # find f_n for every n
+    for j in range(n):
+        Beta, MSE = f(j)
+        draw_graph(j, Beta, MSE)
